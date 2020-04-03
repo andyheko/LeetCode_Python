03bfe11ee07df361884fd5e508f660ef8af9bc26
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-03 17:48:38 +0800

    Update 033_Search_in_Rotated_Sorted_Array.py

--- Searching/033_Search_in_Rotated_Sorted_Array.py
+++ Searching/033_Search_in_Rotated_Sorted_Array.py
@@ -25,22 +25,42 @@ class Solution:
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def searchRecursive(nums, left, right, target):
-        	if left > right:
-        		return -1
-
-        	mid = (left + right) // 2
-        	if nums[mid] == target:
-        		return mid
-
-        	if nums[left] <= nums[mid]:
-        		if nums[left] <= target < nums[mid]:
-        			return searchRecursive(nums, left, mid-1, target)
-        		else:
-        			return searchRecursive(nums, mid+1, right, target)
-        	else:
-        		if nums[mid] < target <= nums[right]:
-        			return searchRecursive(nums, mid+1, right, target)
-        		else:
-        			return searchRecursive(nums, left, mid-1, target)
-
+            if left > right:
+                return -1
+            else:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
+                if nums[right] >= nums[mid]:
+                    if nums[mid] < target <= nums[right]:
+                        return searchRecursive(nums, mid+1, right, target)
+                    else:
+                        return searchRecursive(nums, left, mid-1, target)
+                else:
+                    if nums[left] <= target < nums[mid]:
+                        return searchRecursive(nums, left, mid-1, target)
+                    else:
+                        return searchRecursive(nums, mid+1, right, target)
         return searchRecursive(nums, 0, len(nums)-1, target)
+        
+# Recursive Solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.searchRecursive(nums, 0, len(nums)-1, target)
+    def searchRecursive(self, nums, left, right, target):
+        if left > right:
+            return -1
+        else:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[right] >= nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    return self.searchRecursive(nums, mid+1, right, target)
+                else:
+                    return self.searchRecursive(nums, left, mid-1, target)
+            else:
+                if nums[left] <= target < nums[mid]:
+                    return self.searchRecursive(nums, left, mid-1, target)
+                else:
+                    return self.searchRecursive(nums, mid+1, right, target)

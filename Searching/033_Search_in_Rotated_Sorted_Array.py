84ba918442f16dfa7870fa5cdad224bf9c579e23
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-03 17:12:45 +0800

    Add 33

---
+++ Searching/033_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,26 @@
+
+
+
+# Recursive Solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def searchRecursive(nums, left, right, target):
+        	if left > right:
+        		return -1
+
+        	mid = (left + right) // 2
+        	if nums[mid] == target:
+        		return mid
+
+        	if nums[left] <= nums[mid]:
+        		if nums[left] <= target < nums[mid]:
+        			return searchRecursive(nums, left, mid-1, target)
+        		else:
+        			return searchRecursive(nums, mid+1, right, target)
+        	else:
+        		if nums[mid] < target <= nums[right]:
+        			return searchRecursive(nums, mid+1, right, target)
+        		else:
+        			return searchRecursive(nums, left, mid-1, target)
+
+        return searchRecursive(nums, 0, len(nums)-1, target)
--- Searching/704_Binary_Search.py
+++ Searching/704_Binary_Search.py
@@ -13,3 +13,36 @@ class Solution:
             else:
                 last = mid - 1
         return -1
+
+### Recursive Solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def helper(nums, left, right, target):
+            if left > right:
+                return -1
+            else:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
+                elif target > nums[mid]:
+                    return helper(nums, mid+1, right, target)
+                else:
+                    return helper(nums, left, mid-1, target)
+
+        return helper(nums, 0, len(nums)-1, target)
+
+### Recursive Solution 2
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.helper(nums, 0, len(nums)-1, target)
+    def helper(self, nums, left, right, target):
+        if left > right:
+            return -1
+        else:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                return self.helper(nums, mid+1, right, target)
+            else:
+                return self.helper(nums, left, mid-1, target)

cd16ca919a0450bd609c03376483539e75048c73
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-03 17:37:52 +0800

    Update 033_Search_in_Rotated_Sorted_Array.py

--- Searching/033_Search_in_Rotated_Sorted_Array.py
+++ Searching/033_Search_in_Rotated_Sorted_Array.py
@@ -1,5 +1,25 @@
-
-
+# Iteration Solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        while right >= left:
+            mid = (right + left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[right] >= nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid] > target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
 
 # Recursive Solution
 class Solution:

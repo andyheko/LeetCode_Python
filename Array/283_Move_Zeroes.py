5353705a60788102f38eac229b6f0e5e016af58d
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-04 18:59:18 +0800

    Update 283_Move_Zeroes.py

--- Array/283_Move_Zeroes.py
+++ Array/283_Move_Zeroes.py
@@ -13,4 +13,14 @@ class Solution:
         for k in range(j, len(nums)):
             nums[k] = 0
 
-# 
+# Optimal Solution
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1

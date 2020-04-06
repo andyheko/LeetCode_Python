9cecae3962f55ca389c5247418775e7f445688e8
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-06 16:54:36 +0800

    Update 049_Group_Anagrams.py

--- String/049_Group_Anagrams.py
+++ String/049_Group_Anagrams.py
@@ -4,3 +4,14 @@ class Solution:
         for word in strs:
                 output[tuple(sorted(word))].append(word)
         return output.values()
+
+### Solution 2
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = collections.defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            output[tuple(count)].append(word)
+        return output.values()

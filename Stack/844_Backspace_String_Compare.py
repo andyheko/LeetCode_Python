28107799a1db579bd56815cb7e68ca8f3c4c3597
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-09 23:19:15 +0800

    Update 844_Backspace_String_Compare.py

--- Stack/844_Backspace_String_Compare.py
+++ Stack/844_Backspace_String_Compare.py
@@ -9,3 +9,19 @@ class Solution:
                     ans.append(c)
             return ans
         return build(S) == build(T)
+
+# Solution 2: Two pointer
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def builder(a):
+            skip = 0 # indicate how many character need to be skipped
+            for c in reversed(a):
+                if c == '#':
+                    skip += 1
+                # if encounter '#' previously, skip one character and reset skip
+                elif skip:
+                    skip -= 1
+                else:
+                    yield c
+
+        return all(x == y for x, y in itertools.zip_longest(builder(S), builder(T)))

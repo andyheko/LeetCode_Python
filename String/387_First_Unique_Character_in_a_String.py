de879e992bb8571552a9175a7d718d3ec5f56b5c
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-12 14:21:47 +0800

    Update 387_First_Unique_Character_in_a_String.py

--- String/387_First_Unique_Character_in_a_String.py
+++ String/387_First_Unique_Character_in_a_String.py
@@ -13,4 +13,17 @@ class Solution(object):
         for idx, c in enumerate(s):
             if seen[c] == 1:
                 return idx
+        return -1
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = collections.Counter(s)
+
+        for idx, c in enumerate(s):
+            if count[c] == 1:
+                return idx
         return -1    

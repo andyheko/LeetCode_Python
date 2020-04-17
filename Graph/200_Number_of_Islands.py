1afcd8c29917f8b0748dab1d20db8bb3081191da
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-17 16:34:52 +0800

    Update 200_Number_of_Islands.py

--- Graph/200_Number_of_Islands.py
+++ Graph/200_Number_of_Islands.py
@@ -23,3 +23,29 @@ class Solution(object):
                 if dfs(i,j):
                     count += 1
         return count
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == '0':
+                return
+            # flip card from 1 to 0
+            grid[i][j] = '0'
+            # recursive function all adjacent cards
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # if encounter '1', count +=1, meanwhile fip all adjacent 1 to 0
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count            

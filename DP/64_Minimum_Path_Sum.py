13d5549fcc3a732c07155ec986f2dafdf66414fa
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-20 13:14:41 +0800

    Update 64_Minimum_Path_Sum.py

--- DP/64_Minimum_Path_Sum.py
+++ DP/64_Minimum_Path_Sum.py
@@ -12,12 +12,12 @@ class Solution(object):
 
         for r in range(row):
             for c in range(col):
-                if r == 0 and c == 0:
+                if r == 0 and c == 0: # skip the top left corner
                     continue
-                elif r == 0:
+                elif r == 0: # for top elements
                     grid[r][c] = grid[r][c] + grid[r][c-1]
-                elif c == 0:
+                elif c == 0: # for left elements
                     grid[r][c] = grid[r][c] + grid[r-1][c]
-                else:
+                else: # for other elememts
                     grid[r][c] = grid[r][c] + min(grid[r-1][c], grid[r][c-1])
         return grid[row-1][col-1]

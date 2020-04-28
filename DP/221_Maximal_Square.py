33ded502e84aee46be02925d5da3c15692c957be
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-28 16:51:22 +0800

    Update 221_Maximal_Square.py

--- DP/221_Maximal_Square.py
+++ DP/221_Maximal_Square.py
@@ -1,3 +1,36 @@
+###Brute Force
+class Solution(object):
+    def maximalSquare(self, matrix):
+        if not matrix:
+            return 0
+        current = 0
+        maximum = 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    current = 1
+                    go = 1
+                    while i + current < rows and j + current < cols and go:
+                        for k in range(i, i+current+1):
+                            if matrix[k][j+current] == '0':
+                                go = 0
+                                break
+                        for l in range(j, j+current+1):
+                            if matrix[i+current][l] == '0':
+                                go = 0
+                                break
+                        if go:
+                            current += 1
+                    if current > maximum:
+                        maximum = current
+
+
+
+
+
 class Solution(object):
     def maximalSquare(self, matrix):
         if not matrix:

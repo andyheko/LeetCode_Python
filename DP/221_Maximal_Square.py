7a12d8da6f8caf620934d1d3c4844cd5e0695665
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-28 20:50:49 +0800

    Update 221_Maximal_Square.py

--- DP/221_Maximal_Square.py
+++ DP/221_Maximal_Square.py
@@ -28,22 +28,45 @@ class Solution(object):
                         maximum = current
 
 
-
-
-
+### Dynamic Programming
 class Solution(object):
     def maximalSquare(self, matrix):
         if not matrix:
             return 0
-        m , n = len(matrix), len(matrix[0])
-        dp = [[ 0 if matrix[i][j] == '0' else 1 for j in range(0, n)] for i in range(0, m)]
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        for i in range(1, m):
-            for j in range(1, n):
+        dp = [[0 if matrix[i][j] == '0' else 1 for j in range(cols)]for i in range(rows)]
+
+        for i in range(1, rows):# caution: starts from 1
+            for j in range(1, cols):# caution: starts from 1
                 if matrix[i][j] == '1':
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
                 else:
                     dp[i][j] = 0
+        output = max(max(row) for row in dp)
 
-        res = max(max(row) for row in dp)
-        return res ** 2
+        return output ** 2
+
+### DP with Space complexity O(1)
+class Solution(object):
+    def maximalSquare(self, matrix):
+        if not matrix:
+            return 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for i in range(0, rows):
+            for j in range(0, cols):
+                if i == 0 or j == 0:
+                    if matrix[i][j] == '1':
+                        matrix[i][j] = 1
+                    else:
+                        matrix[i][j] = 0
+                elif matrix[i][j] == '1':
+                    matrix[i][j] = min(matrix[i-1][j], matrix[i-1][j-1], matrix[i][j-1]) + 1
+                else:
+                    matrix[i][j] = 0
+        output = max(max(row) for row in matrix)
+
+        return output ** 2

e1d0658265023987051ac536c83a523c2ad73e9e
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-01 23:26:00 +0800

    Create 349_Intersection of Two Arrays.py

---
+++ Hash/349_Intersection of Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        if len(set1) < len(set2):
+            return [x for x in set1 if x in set2]
+        else:
+            return [x for x in set2 if x in set1]

4e96301b09922b0ef1db9696215d314657fb95c6
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-03 23:54:08 +0800

    Update 692_Top K Frequent Words.py

--- Hash/692_Top K Frequent Words.py
+++ Hash/692_Top K Frequent Words.py
@@ -2,5 +2,5 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         count = collections.Counter(words)
         ans = count.keys()
-        ans = sorted(ans, key = lambda w: (-count[w], w))
+        ans = sorted(ans, key = lambda w: (-count[w], w)) # -count[w] means reversed sorting of count frequendy, w means when frequency is equal, compare in alphabetical order
         return ans[:k]

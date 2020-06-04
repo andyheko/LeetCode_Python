6ee8f10e48c7d1f4f478e5730db86f96adb37539
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-04 20:54:57 +0800

    Update 692_Top K Frequent Words.py

--- Hash/692_Top K Frequent Words.py
+++ Hash/692_Top K Frequent Words.py
@@ -4,3 +4,10 @@ class Solution:
         ans = count.keys()
         ans = sorted(ans, key = lambda w: (-count[w], w)) # -count[w] means reversed sorting of count frequendy, w means when frequency is equal, compare in alphabetical order
         return ans[:k]
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = collections.Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap) # turn a list into a heap in linear time
+        return [heapq.heappop(heap)[1] for _ in range(k)]

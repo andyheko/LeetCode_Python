d0655a15728bf40481106d3b6d8e7565ce6868a3
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-26 08:16:03 -0700

    Create 380_Insert Delete GetRandom O(1).py

---
+++ Design/380_Insert Delete GetRandom O(1).py
@@ -0,0 +1,39 @@
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.list = []
+        self.dict = {}
+
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            last_element, idx = self.list[-1], self.dict[val]
+            # swap remove element ans last element in list
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete remove element
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)

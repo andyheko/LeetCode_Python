8eaf215fca4d62f93ac777d9f5a7b54b1878b454
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-27 14:48:07 -0700

    Create 289_Game of Life.py

---
+++ Array/289_Game of Life.py
@@ -0,0 +1,28 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        rows = len(board)
+        cols = len(board[0])
+        copy_board = deepcopy(board)
+        #copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        for row in range(rows):
+            for col in range(cols):
+                live_neighbors = 0
+                for neighbor in neighbors:
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    if (0 <= r < rows) and (0 <= c < cols) and (copy_board[r][c] == 1):
+                        live_neighbors += 1
+                # Rule 1 or Rule 3
+                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = 0
+                # Rule 4
+                if copy_board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 1

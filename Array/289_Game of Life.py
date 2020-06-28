7757af9bf8d9ce264750d461f4d1392edffe5e48
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-28 11:57:28 -0700

    Update 289_Game of Life.py

--- Array/289_Game of Life.py
+++ Array/289_Game of Life.py
@@ -26,3 +26,35 @@ class Solution:
                 # Rule 4
                 if copy_board[row][col] == 0 and live_neighbors == 3:
                     board[row][col] = 1
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        rows = len(board)
+        cols = len(board[0])
+
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        for row in range(rows):
+            for col in range(cols):
+                live_neighbors = 0
+                for neighbor in neighbors:
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    if (0 <= r < rows) and (0 <= c < cols) and (board[r][c] == 1):
+                        live_neighbors += 1
+                # Rule 1 or Rule 3
+                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = -1
+                # Rule 4
+                if copy_board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 2
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0

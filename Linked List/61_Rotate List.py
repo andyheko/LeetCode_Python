fa74c665284d7a62547dd895efabf1221f1d75a8
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-08-24 09:16:20 -0700

    update

---
+++ Linked List/61_Rotate List.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head:
+            return None
+        if not head.next:
+            return head
+
+        # link tail to head
+        old_tail = head
+        n = 1
+        while old_tail.next:
+            old_tail = old_tail.next
+            n += 1
+        old_tail.next = head
+
+        # find new tail
+        new_tail = head
+        for i in range(n - k % n - 1): # new_tail == (n-k%n)th node
+            new_tail = new_tail.next
+        new_head = new_tail.next
+
+        # break the ring
+        new_tail.next = None
+
+        return new_head

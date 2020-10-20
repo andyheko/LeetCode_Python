9214a399ee24fe04f667a93233f333d9b577a6c7
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-10-19 17:37:09 -0700

    Update 443. String Compression.py

--- String/443. String Compression.py
+++ String/443. String Compression.py
@@ -1,5 +1,24 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        left, i = 0, 0 # left is new chars idx
+        while i < len(chars):
+            curr = chars[i]
+            count = 0
+            while i < len(chars) and curr == chars[i]: # start count
+                count += 1
+                i += 1
+            chars[left] = curr
+            left += 1 # left is new chars idx
+            if count > 1:
+                for c in str(count): # count could > 9
+                    chars[left] = c
+                    left += 1
+        chars[:] = chars[:left]
+        print(chars)
+        return len(chars)
+        
+class Solution:
+    def compress(self, chars: List[str]) -> int:
         if len(chars) <= 1:
             return len(chars)
         ans = [chars[0]]
